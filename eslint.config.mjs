8f34b99ca189d4341afcd8e6bbaf9cd341c378de
reollaereoru-lgtm <reollaereoru@gmail.com>
2026-07-31 20:08:13 +0800

    eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@next': nextPlugin
+    },
+    rules: {
+      '@next/no-img-element': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    }
+  }
+);
